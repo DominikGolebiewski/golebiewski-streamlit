e58d28eb71eaa1792ac94654c7c8fa6fb49b8e79
Dominik Golebiewski <dominik.golebiewski@atheon.co.uk>
2023-08-22 16:36:14 +0100

    Add menu

--- pages/4_Elements_UI.py
+++ pages/4_Elements_UI.py
@@ -12,24 +12,10 @@ from pathlib import Path
 from streamlit_elements import dashboard, elements, mui, nivo
 
 with elements("bar"):
-    with mui.AppBar(position="static", sx={"backgroundColor": "#fff"}):
-        with mui.Container( maxWidth="xl"):
-            with mui.Toolbar(disableGutters=True):
-                mui.Typography(
-                    "LOGO",
-                    variant="h6",
-                    noWrap=True,
-                    component = "a",
-                    # href = "/",
-                    sx = {
-                        'mr': 2,
-                        # 'display': {'xs': 'none', 'md': 'flex'},
-                        'fontFamily': 'monospace',
-                        'fontWeight': 700,
-                        'letterSpacing': '.3rem',
-                        'color': 'black',
-                        'textDecoration': 'none',
-                    }
-                )
-                with mui.Box(sx = {'flexGrow': 1, 'display': {'xs': 'flex', 'md': 'none'}}):
-                    mui.IconButton(size = "large",aria_label = "account of current user",aria_controls = "menu-appbar",aria_haspopup = "true",color = "inherit")
+    with mui.Box(sx={ 'flexGrow': 1 }):
+        with mui.AppBar( position="static"):
+            with mui.Toolbar():
+                with mui.IconButton(size = "large",aria_label = "account of current user",aria_controls = "menu-appbar",aria_haspopup = "true",color = "inherit"):
+                    mui.icon.Menu(sx = {'color': 'white'})
+                mui.Typography("DBT Elements", variant="h6", component="div", sx={ 'flexGrow': 1 })
+                mui.Button("Login", color="inherit", onClick=lambda: st.write("Clicked!"), sx={ 'color': 'white' }, formattext="none", variant="text")
